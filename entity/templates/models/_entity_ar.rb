eadac73e1374ae45ac663bdf09f6dab645fad726
Robert Yokota <ryokota@yammer-inc.com>
2014-01-25 18:54:50 -0800

    Add ActiveRecord validations

--- entity/templates/models/_entity_ar.rb
+++ entity/templates/models/_entity_ar.rb
@@ -1,2 +1,6 @@
 class <%= _.capitalize(name) %> < ActiveRecord::Base
+  <% _.each(attrs, function (attr) { %>
+  <% if (attr.required) { %>validates :<%= attr.attrName %>, presence: true<% }; %>
+  <% if (attr.maxLength) { %>validates :<%= attr.attrName %>, length: { <% if (attr.minLength) { %>minimum: <%= attr.minLength %>, <% } %>maximum: <%= attr.maxLength %> }<% };%>
+  <% }); %>
 end
